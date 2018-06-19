53073ef9077193c1430495b42b5b14a61b9a0479
juanfsrv <31480103+juanfsrv@users.noreply.github.com>
2018-06-18 23:41:21 -0300

    adicionado busca por cidade

--- app/App.js
+++ app/App.js
@@ -1,55 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 
-import Weather from './components/Weather';
+import Tempo from './components/Tempo';
 
 export default class App extends React.Component {
   state = {
     carregando: true,
-    temperatura: 0,
-    condicoesTempo: null,
-    error: null
   };
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        this.obterTempo(position.coords.latitude, position.coords.longitude);
-      },
-      error => {
-        this.setState({
-          error: 'Erro obtendo o Tempo'
-        });
-      }
-    );
-  }
-
-  obterTempo(lat, lon) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
-    )
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          temperatura: json.main.temp,
-          condicoesTempo: json.weather[0].main,
-          carregando: false
-        });
-      });
-  }
 
   render() {
     const { carregando, condicoesTempo, temperatura } = this.state;
-    const API_KEY = 'ffce6245c935c0052b3609b1a83510a1';
     return (
       <View style={styles.container}>
-        {carregando ? (
+        {/* {carregando ? (
           <View style={styles.carregandoContainer}>
             <Text style={styles.carregandoText}>Carregando o Tempo...</Text>
           </View>
         ) : (
-          <Weather tempo={condicoesTempo} temperatura={temperatura} />
-        )}
+          <Tempo/>
+        )} */}
+        <Tempo/>
       </View>
     );
   }
--- app/components/Tempo.js
+++ app/components/Tempo.js
@@ -1,39 +1,90 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import PropTypes from 'prop-types';
-import { condicoesTempo } from '../CondicoesTempo';
+import { condicoesTempo } from '../components/CondicoesTempo';
 
-const Tempo = ({ tempo, temperatura }) => {
-    return (
-        <View
-            style={[
-                styles.tempoContainer,
-                { backgroundColor: condicoesTempo[tempo].color }
-            ]}
-        >
-            <View style={styles.headerContainer}>
-                <MaterialCommunityIcons
-                    size={72}
-                    name={condicoesTempo[tempo].icon}
-                    color={'#fff'}
-                />
-                <Text style={styles.tempText}>{temperatura}˚</Text>
-            </View>
-            <View style={styles.bodyContainer}>
-                <Text style={styles.title}>{condicoesTempo[tempo].title}</Text>
-                <Text style={styles.subtitle}>
-                    {condicoesTempo[tempo].subtitle}
-                </Text>
-            </View>
-        </View>
-    );
-};
+export default class Tempo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            text: 'Salvador',
+            temperatura: 0,
+            minTemp: 0,
+            maxTemp: 0,
+            condicoesTempo: 'Rain',
+            cidade: null,
+            pais: null,
+            error: null
+        }
+    }
 
-Tempo.propTypes = {
-    temperatura: PropTypes.number.isRequired,
-    tempo: PropTypes.string
-};
+    componentDidMount() {
+        this.obterTempo(this.state.text);
+      }
+
+    render() {
+        return (
+            <View
+                style={[
+                    styles.tempoContainer,
+                    { backgroundColor: condicoesTempo[this.state.condicoesTempo].color }
+                ]}
+            >
+                <View style={styles.headerContainer}>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Informe uma cidade..."
+                        placeholderTextColor="#fff"
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.text}
+                    />
+                    <Button
+                        color="#fff"
+                        onPress={() => this.obterTempo(this.state.text)}
+                        title="Buscar"
+                    />
+                </View>
+                <View style={styles.tempContainer}>
+                    <MaterialCommunityIcons
+                        size={72}
+                        name={condicoesTempo[this.state.condicoesTempo].icon}
+                        color={'#fff'}
+                    />
+                    <Text style={styles.tempText}>{this.state.temperatura}˚</Text>
+                </View>
+                <View style={styles.localContainer}>
+                    <Text style={{fontSize: 20, color: '#fff'}}>Min {this.state.minTemp}˚ /  Máx{this.state.maxTemp}˚</Text>
+                    <Text style={styles.localText}>{this.state.cidade}, {this.state.pais}</Text>
+                </View>
+                <View style={styles.bodyContainer}>
+                    <Text style={styles.title}>{condicoesTempo[this.state.condicoesTempo].title}</Text>
+                    <Text style={styles.subtitle}>
+                        {condicoesTempo[this.state.condicoesTempo].subtitle}
+                    </Text>
+                </View>
+            </View>
+        );
+    };
+
+    obterTempo(cidade) {
+        const API_KEY = '19607e6ea7ab40f6690269ad2ecb9cf0';
+        fetch(
+          `http://api.openweathermap.org/data/2.5/weather?q=${cidade}&APPID=${API_KEY}&units=metric`
+        )
+          .then(res => res.json())
+          .then(json => {
+            console.log(json);
+            this.setState({
+              temperatura: json.main.temp,
+              minTemp: json.main.temp_min,
+              maxTemp: json.main.temp_max,
+              condicoesTempo: json.weather[0].main,
+              cidade: json.name,
+              pais: json.sys.country
+            });
+          });
+      }
+}
 
 const styles = StyleSheet.create({
     tempoContainer: {
@@ -41,6 +92,11 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         flex: 1,
+        alignItems: 'center',
+        marginTop: 80
+    },
+    tempContainer: {
+        flex: 0,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around'
@@ -49,8 +105,18 @@ const styles = StyleSheet.create({
         fontSize: 72,
         color: '#fff'
     },
+    localContainer: {
+        flex: 0,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    localText: {
+        fontSize: 40,
+        color: '#fff'
+    },
     bodyContainer: {
-        flex: 2,
+        flex: 1,
         alignItems: 'flex-start',
         justifyContent: 'flex-end',
         paddingLeft: 25,
@@ -63,7 +129,15 @@ const styles = StyleSheet.create({
     subtitle: {
         fontSize: 24,
         color: '#fff'
-    }
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#fff',
+        borderRadius: 8,
+        padding: 5,
+        height: 40,
+        width: 220,
+        fontSize: 20,
+        color: '#fff'
+      }
 });
-
-export default Weather;
